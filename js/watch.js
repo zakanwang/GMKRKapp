51a25e63008cbaa5a464c18064199090486eab69
a1923ht <a1923ht@aiit.ac.jp>
2020-04-08 21:52:53 +0900

    remove mSec and add hour valuse

--- js/watch.js
+++ js/watch.js
@@ -28,24 +28,29 @@
                 time++;
 
                 // 分・秒・ミリ秒を計算
+                var hour = Math.floor(time/100/60/60);
                 var min = Math.floor(time/100/60);
                 var sec = Math.floor(time/100);
                 var mSec = time % 100;
+
+                
+                // 分が６０分以上の場合 例）89分→29分にする
+                if (min >= 60) min = min % 60;
 
                 // 分が１桁の場合は、先頭に０をつける
                 if (min < 10) min = "0" + min;
 
                 // 秒が６０秒以上の場合　例）89秒→29秒にする
                 if (sec >= 60) sec = sec % 60;
-
+                
                 // 秒が１桁の場合は、先頭に０をつける
                 if (sec < 10) sec = "0" + sec;
 
-                // ミリ秒が１桁の場合は、先頭に０をつける
-                if (mSec < 10) mSec = "0" + mSec;
+
+
 
                 // タイマーラベルを更新
-                timerLabel.innerHTML = min + ":" + sec + ":" + mSec;
+                timerLabel.innerHTML = hour + ":" + min + ":" + sec;
 
                 // 再びtimer()を呼び出す
                 timer();
